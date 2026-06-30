3608fbd735f37832ef6dcc848d79b0567cd358ad
Isa Yan <isabel.yyj.yan@gmail.com>
2026-06-30 16:21:08 -0400

    setup idk i fucking hate no wifi goddamit i suck at coding i should kms or give up either works i genuinely am a rat bastard

--- rpg.cpp
+++ rpg.cpp
@@ -3,3 +3,27 @@
 #include <random>
 #include <algorithm>
 #include <iostream>
+
+/*
+clang++ -g rpg.cpp -o rpg
+./rpg
+
+*/
+
+int main(){
+    //setup player, health, and inventory
+    int player = true;
+    int health = 100;   
+    char keydown;
+    std::vector<int> inventory;
+    inventory.push_back(4);
+    std::cin>>keydown;
+    if (keydown=='i'){
+        std::cout<<"you have "<<"\n";
+        for (int i=0;inventory[i];i++){
+            std::cout<<inventory[i]<<", ";
+        }
+    }
+
+    return 0;
+}
