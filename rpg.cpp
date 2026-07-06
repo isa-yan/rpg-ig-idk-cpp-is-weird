e44ed72c173c4ea2525db30d9a8be337bc05f66e
Isa Yan <isabel.yyj.yan@gmail.com>
2026-07-07 03:23:30 +0800

    oooh i have internet now i understand how cpp works :3c

--- rpg.cpp
+++ rpg.cpp
@@ -3,10 +3,11 @@
 #include <random>
 #include <algorithm>
 #include <iostream>
+using namespace std;
 //#include <tuple>
 
 /*
-clang++ -g rpg.cpp -o rpg
+clang++ -std=c++17 -g rpg.cpp -o rpg
 ./rpg
 
 */
@@ -15,42 +16,42 @@ int main(){
     //setup player, health, and inventory
         int health = 100;   
         char keydown;
-        std::string player_name;
+        string player_name;
         //std::tuple<std::string, int> player();
-        std::vector<int> inventory;
+        vector<int> inventory;
         if (keydown=='i'){
-            std::cout<<"you have "<<"\n";
+            ::cout<<"you have "<<"\n";
             for (int i=0;inventory[i];i++){
-                std::cout<<inventory[i]<<", ";
+                cout<<inventory[i]<<", ";
             }
         }
 
     //introduction
         char start;
-        std::cout<<"what is your name gangsta?"<<"\n";
-        std::cin>>player_name;
-        std::cout<<"hello "<<player_name<<" to the world of 1920s Chicago"<<"\n";
-        std::cout<<"here you'll face gangs, mobsters, and crime. also a lot a alcohol"<<"\n";
-        std::cout<<"this'll be the start of your journey. whether you decide to be an incorruptible bastion of good or a cop with their own agenda, that is up for you to decide"<<"\n";
-        std::cout<<"if you want to start right now, click 'y'. otherwise click 'n'"<<"\n";
-        std::cin>>start;
+        cout<<"what is your name gangsta?"<<"\n";
+        cin>>player_name;
+        cout<<"hello "<<player_name<<" to the world of 1920s Chicago"<<"\n";
+        cout<<"here you'll face gangs, mobsters, and crime. also a lot a alcohol"<<"\n";
+        cout<<"this'll be the start of your journey. whether you decide to be an incorruptible bastion of good or a cop with their own agenda, that is up for you to decide"<<"\n";
+        cout<<"if you want to start right now, click 'y'. otherwise click 'n'"<<"\n";
+        cin>>start;
         switch(start){
             case 'n':
                 break;
             case 'y':
-                std::cout<<"let's go officer. time to go back home to Chicago"<<"\n";
+                cout<<"let's go officer. time to go back home to Chicago"<<"\n";
         }
 
     //story start
-        std::vector<std::string> script_A;
-        script_A.push_back("You open your eyes to find yourself in the police station");
-        script_A.push_back("meow");
-            for (int i=0;script_A[i].empty()==false;i++){
-                std::cout<<script_A[i]<<", ";
-            }
+        vector<string> script_A = {"You open your eyes to find yourself in the police station","it's been a hard time for you lately","crime has been spreading throughout Chicago"};
+            for (int i=0;i<script_A.size();i++){
+                cin>>keydown;
+                if (keydown=='e'){
+                    cout<<script_A[i]<<"\n";
+                }
+            }   
+        
 
-        
-        
 
     return 0;
 }
